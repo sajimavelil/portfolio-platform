b7a5efef6ac26bc5edc986dd371c8834372dad15
Saji Joseph <sajimavelil@gmail.com>
2026-06-28 11:44:15 +0530

    Add BaseEntity and JPA auditing

---
+++ backend/src/main/java/com/sajijoseph/portfoliocms/common/config/JpaAuditConfig.java
@@ -0,0 +1,9 @@
+package com.sajijoseph.portfolio.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
---
+++ backend/src/main/java/com/sajijoseph/portfoliocms/common/entity/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.sajijoseph.portfolio.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+}
